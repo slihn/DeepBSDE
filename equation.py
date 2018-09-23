9b6c7aa2bd5f7ef3784c35da0d06a24eb6ced040
Stephen Lihn <stevelihn@gmail.com>
2018-09-23 11:49:16 -0400

    renames num_sample to batch_size to be more intuitive with the paper

--- equation.py
+++ equation.py
@@ -24,7 +24,7 @@ class Equation(object):
         self._sigma = np.sqrt(2.0)
         self._lambda = 1.0
 
-    def sample(self, num_sample):
+    def sample(self, batch_size):
         """Sample forward SDE."""
         raise NotImplementedError
 
@@ -56,24 +56,24 @@ class Equation(object):
     def delta_t(self):
         return self._delta_t
 
-    def dw_sample_normal(self, num_sample):
-        dw_sample = normal.rvs(size=[num_sample,
+    def dw_sample_normal(self, batch_size):
+        dw_sample = normal.rvs(size=[batch_size,
                                      self._dim,
                                      self._num_time_interval]) * self._sqrt_delta_t
         return dw_sample
 
-    def x_sample_bm(self, num_sample, dw_sample):
+    def x_sample_bm(self, batch_size, dw_sample):
         # X(t) = vectorize(self._x_init)
         # X(t+1) = X(t) + sigma * dW(t), see Section 5.4 of [1]
-        x_sample = np.zeros([num_sample, self._dim, self._num_time_interval + 1])
-        x_sample[:, :, 0] = np.ones([num_sample, self._dim]) * self._x_init
+        x_sample = np.zeros([batch_size, self._dim, self._num_time_interval + 1])
+        x_sample[:, :, 0] = np.ones([batch_size, self._dim]) * self._x_init
         for i in xrange(self._num_time_interval):
             x_sample[:, :, i + 1] = x_sample[:, :, i] + self._sigma * dw_sample[:, :, i]
         return x_sample
 
-    def sample_bm_normal(self, num_sample):
-        dw_sample = self.dw_sample_normal(num_sample)
-        x_sample = self.x_sample_bm(num_sample, dw_sample)
+    def sample_bm_normal(self, batch_size):
+        dw_sample = self.dw_sample_normal(batch_size)
+        x_sample = self.x_sample_bm(batch_size, dw_sample)
         return dw_sample, x_sample
 
 
@@ -92,8 +92,8 @@ class AllenCahn(Equation):
     def __init__(self, dim, total_time, num_time_interval):
         super(AllenCahn, self).__init__(dim, total_time, num_time_interval)
 
-    def sample(self, num_sample):
-        return self.sample_bm_normal(num_sample)
+    def sample(self, batch_size):
+        return self.sample_bm_normal(batch_size)
 
     def f_tf(self, t, x, y, z):
         return y - tf.pow(y, 3)  # Section 4.2, page 12 of [1]
@@ -106,8 +106,8 @@ class HJB(Equation):
     def __init__(self, dim, total_time, num_time_interval):
         super(HJB, self).__init__(dim, total_time, num_time_interval)
 
-    def sample(self, num_sample):
-        return self.sample_bm_normal(num_sample)
+    def sample(self, batch_size):
+        return self.sample_bm_normal(batch_size)
 
     def f_tf(self, t, x, y, z):
         return -self._lambda * rsum(tf.square(z))  # Section 4.3, page 13 of [1]
@@ -126,13 +126,13 @@ class PricingOption(Equation):
         self._rb = 0.06  # R_b: borrow rate
         self._alpha = 1.0 / self._dim
 
-    def sample(self, num_sample):
-        dw_sample = self.dw_sample_normal(num_sample)
+    def sample(self, batch_size):
+        dw_sample = self.dw_sample_normal(batch_size)
 
         # (52) of [1], gbm, X(t+1) = e^(sigma dW(t) + drift) * X(t)
         # drift = (mu - sigma^2/2) dt
-        x_sample = np.zeros([num_sample, self._dim, self._num_time_interval + 1])
-        x_sample[:, :, 0] = np.ones([num_sample, self._dim]) * self._x_init
+        x_sample = np.zeros([batch_size, self._dim, self._num_time_interval + 1])
+        x_sample[:, :, 0] = np.ones([batch_size, self._dim]) * self._x_init
         drift = (self._mu_bar-(self._sigma**2)/2) * self._delta_t
         for i in xrange(self._num_time_interval):
             x_sample[:, :, i + 1] = np.exp(self._sigma * dw_sample[:, :, i] + drift) * x_sample[:, :, i]
@@ -163,13 +163,13 @@ class PricingDefaultRisk(Equation):
         self._vl = 70.0
         self._slope = (self._gammah - self._gammal) / (self._vh - self._vl)
 
-    def sample(self, num_sample):
-        dw_sample = self.dw_sample_normal(num_sample)
+    def sample(self, batch_size):
+        dw_sample = self.dw_sample_normal(batch_size)
 
         # TODO: the following needs a reference?
         # it seems to be X(t+1) = (1+ mu*dt + sigma dW(t)) X(t)
-        x_sample = np.zeros([num_sample, self._dim, self._num_time_interval + 1])
-        x_sample[:, :, 0] = np.ones([num_sample, self._dim]) * self._x_init
+        x_sample = np.zeros([batch_size, self._dim, self._num_time_interval + 1])
+        x_sample[:, :, 0] = np.ones([batch_size, self._dim]) * self._x_init
         for i in xrange(self._num_time_interval):
             x_sample[:, :, i + 1] = (1 + self._mu_bar * self._delta_t) * x_sample[:, :, i] + (
                 self._sigma * x_sample[:, :, i] * dw_sample[:, :, i])
@@ -191,8 +191,8 @@ class BurgesType(Equation):
         self._sigma = self._dim * 1.0 # this is different from what's in page 20 of [1]
         # self._sigma = self._dim / np.sqrt(2.0) # changed from d to d/sqrt(2) to match what's in page 20 of [1]
 
-    def sample(self, num_sample):
-        return self.sample_bm_normal(num_sample)
+    def sample(self, batch_size):
+        return self.sample_bm_normal(batch_size)
 
     def f_tf(self, t, x, y, z):  # (57) in Section 4.3 of [1]
         return (y - (2 + self._dim) / 2.0 / self._dim) * rsum(z)
@@ -211,8 +211,8 @@ class QuadraticGradients(Equation):
         base = self._total_time + np.sum(np.square(self._x_init) / self._dim)
         self._y_init = np.sin(np.power(base, self._alpha))  # this is just sin(1) = 0.84147
 
-    def sample(self, num_sample):
-        return self.sample_bm_normal(num_sample)
+    def sample(self, batch_size):
+        return self.sample_bm_normal(batch_size)
 
     def f_tf(self, t, x, y, z):  # (75), Section 4.6 in [1]
         x_square = rsum(tf.square(x))
@@ -250,8 +250,8 @@ class ReactionDiffusion(Equation):
         sin_term = np.sin(self._lambda * np.sum(self._x_init))
         self._y_init = 1 + self._kappa + sin_term * exp_term  # from (79)
 
-    def sample(self, num_sample):
-        return self.sample_bm_normal(num_sample)
+    def sample(self, batch_size):
+        return self.sample_bm_normal(batch_size)
 
     def f_tf(self, t, x, y, z):  # (77) on page 25 of [1]
         exp_term = tf.exp((self._lambda ** 2) * self._dim * (t - self._total_time) / 2)
